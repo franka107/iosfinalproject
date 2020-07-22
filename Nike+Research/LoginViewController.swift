7d3039eeeaca3d29b11489d89489430ee9622049
Mathi03 <mathi031@hotmail.com>
2020-07-21 20:07:45 -0700

    login

---
+++ Nike+Research/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  Nike+Research
+//
+//  Created by Mathi03 on 7/21/20.
+//  Copyright © 2020 Developers Academy. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var correoField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginAction(_ sender: Any) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
