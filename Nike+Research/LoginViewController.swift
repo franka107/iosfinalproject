4fd433b0695126aa51707b166f384bd7465fa496
Mathi03 <mathi031@hotmail.com>
2020-07-21 20:56:49 -0700

    ignore

--- Nike+Research/LoginViewController.swift
+++ Nike+Research/LoginViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
@@ -20,7 +22,30 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func loginAction(_ sender: Any) {
-        
+        Auth.auth().signIn(withEmail: correoField.text!, password: passwordField.text!) { (user, error) in
+            print("Intentando Iniciar Sesion")
+            if error != nil{
+                print("Se presento el siguiente error: \(error!)")
+                let alerta = UIAlertController(title: "Iniciando Sesion", message: "El usuario no existe. Intente registrarse o introduzca bien sus credenciales.", preferredStyle: .alert)
+                let btnCrear = UIAlertAction(title: "Crear", style: .default, handler: {(UIAlertAction) in
+//                    self.performSegue(withIdentifier: "registrarSegue", sender: nil)
+                })
+                let btnCancelar = UIAlertAction(title: "Cancelar", style: .default, handler: nil)
+                alerta.addAction(btnCrear)
+                alerta.addAction(btnCancelar)
+                self.present(alerta, animated: true, completion: nil)
+            }else{
+                print("Inicio de sesion exitoso")
+//                self.performSegue(withIdentifier: "iniciarsesionsegue", sender: nil)
+                let alerta = UIAlertController(title: "Iniciando Sesion", message: "Te loguearste con Exito", preferredStyle: .alert)
+
+                //                    self.performSegue(withIdentifier: "registrarSegue", sender: nil)
+
+                let btnOK = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alerta.addAction(btnOK)
+                self.present(alerta, animated: true, completion: nil)
+            }
+        }
     }
     
     /*
